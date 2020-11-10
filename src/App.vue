29d086bebf2b0bdae95bd0ee756c145430c28db6
yujou0 <abc83327@gmail.com>
2020-11-10 10:51:53 +0800

    完成商品頁

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,15 @@
       router-link(to='/about') About
     router-view
 </template>
+<template lang="pug">
+  #app
+    b-container
+      b-row
+        b-col.text-center(col="12")
+          #nav
+            router-link(to='/') Home
+            | &nbsp;|&nbsp;
+            router-link(to='/about') About
+        b-col(cols="12")
+          router-view
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,41 @@
 <template lang="pug">
   #home
+    b-row
+      b-col(cols="12" md="6" lg="3" v-for="(item, index) in items" :key="index")
+        b-card(:title="item.name" :img-src="item.img" img-top :sub-title="'$'+item.price")
+          b-button(variant="primary")
+            font-awesome-icon(:icon="['fas','shopping-cart']")
+            | &nbsp;加入購物車
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      items: [
+        {
+          img: 'https://c.ecimg.tw/items/DGAGB4A900ANM1R/000002_1604283032.jpg',
+          name: 'SAMSUNG 256GB記憶卡',
+          price: 788
+        },
+        {
+          img: 'https://c.ecimg.tw/items/DYAN70A900AUPA5/000002_1599102543.jpg',
+          name: '小米手環 5',
+          price: 845
+        },
+        {
+          img: 'https://f.ecimg.tw/items/DYAJCHA900AP7EG/000002_1604373280.jpg',
+          name: 'AirPods Pro',
+          price: 6090
+        },
+        {
+          img: 'https://f.ecimg.tw/items/DYAJ93A900AXPNV/000002_1603176778.jpg',
+          name: 'Apple iPhone 12',
+          price: 28500
+        }
+      ]
+    }
+  }
 }
 </script>
