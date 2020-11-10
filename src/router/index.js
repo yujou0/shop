c4cdc842104546e9e818740a154d1b2eeac203a8
yujou0 <abc83327@gmail.com>
2020-11-10 09:31:57 +0800

    123

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '網路行銷購物網'
+    }
   },
   {
     path: '/about',
@@ -16,12 +19,30 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: '關於網路行銷購物網'
+    }
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import(/* webpackChunkName: "cart" */ '../views/Cart.vue'),
+    meta: {
+      title: '我的購物車'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 進入每個頁面後
+// to 去哪裡
+// from 是從哪來
+router.afterEach((to, from) => {
+  document.title = to.meta.title
 })
 
 export default router
